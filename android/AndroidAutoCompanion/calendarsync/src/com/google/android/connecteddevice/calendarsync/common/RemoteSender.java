575c59a63faa55441be09221d50f6c161a425ff8
Anthony Chen <ajchen@google.com>
2021-10-08 13:39:21 -0700

    Remove Android code.
    The Android code has been moved to
    https://github.com/google/android-auto-companion-android.

--- android/AndroidAutoCompanion/calendarsync/src/com/google/android/connecteddevice/calendarsync/common/RemoteSender.java
+++
@@ -1,7 +0,0 @@
-package com.google.android.connecteddevice.calendarsync.common;
-
-/** A connection used to send calendar sync messages to any remote device. */
-public interface RemoteSender {
-  /** Sends a message to the given remote device. */
-  void send(String deviceId, byte[] message);
-}
--- android/AndroidAutoCompanion/calendarsync/src/com/google/android/connecteddevice/calendarsync/common/package-info.java
+++
@@ -1,5 +0,0 @@
-// Shorten class names in generated Objective-C code.
-@ObjectiveCName("CS")
-package com.google.android.connecteddevice.calendarsync.common;
-
-import com.google.j2objc.annotations.ObjectiveCName;
